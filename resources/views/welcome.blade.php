575fa11045143457eb9cedd99a3e873aaa24d7c1
nemanjamil <nemanjamil@gmail.com>
2018-07-12 21:30:38 +0200

    welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel <?php echo "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"; ?>
                 </div>
 
                 <div class="links">
